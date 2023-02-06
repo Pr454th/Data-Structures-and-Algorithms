035b5578c2e8585ee99657374b6c33f21ce241b5
Pr454th <prasathkarthiban1999@gmail.com>
2023-02-06 19:20:53 +0530

    merge sort

---
+++ Searching and Sorting/gcd.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+    public int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        return gcd(nums[0],nums[nums.length-1]);
+    }
+}
